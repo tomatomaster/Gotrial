c27ad934b226520eeea76355b9dfcfd97431dc1d
tomatomaster <iiitomatomasteriii@gmail.com>
2017-03-26 16:04:44 +0900

    commit ch05

---
+++ ch01/ex05/lissajous.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"image/color"
+	"math/rand"
+	"os"
+	"time"
+
+	"./anime"
+)
+
+var palette = []color.Color{
+	color.RGBA{0x00, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0xff, 0x00, 0xff}}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	anime.Lissajous(os.Stdout, palette)
+}
