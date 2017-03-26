685cc115e6924b76c443bd91fed2b181b0524919
tomatomaster <iiitomatomasteriii@gmail.com>
2017-03-26 15:35:39 +0900

    リサージュ曲線の例を写経&バイナリファイルをコミットしないようにgitignore修正

---
+++ ch01/ex04/dup2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+		}
+		countLines(os.Stderr, counts)
+		f.Close()
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		fmt.Println(input.Text())
+		counts[input.Text()]++
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "reading standard input:", err)
+	}
+}
