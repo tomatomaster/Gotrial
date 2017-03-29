a84ca189128c8801ce8ebf6ab45f0f34118b20d1
tomatomaster <iiitomatomasteriii@gmail.com>
2017-03-29 18:20:08 +0900

    commit ex1.10

---
+++ ch01/ex10/fetchall.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	fName := getHostName(url)
+	oPath := filepath.Join(getCurrentPath(), fName)
+	file, err := os.Create(oPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nbytes, err := io.Copy(file, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+func getCurrentPath() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir
+}
+
+func getHostName(s string) string {
+	fName := strings.TrimPrefix(s, "http://")
+	fName = strings.TrimPrefix(fName, "https://")
+	fName = strings.Split(fName, ".")[0]
+	return fName
+}
