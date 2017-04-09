412dbf1298e681cf6ccfb9db69368ae145b11858
tomatomaster <iiitomatomasteriii@gmail.com>
2017-04-09 23:32:11 +0900

    modify test code ch02/ex04 &  05

--- ch02/ex05/popcount_test.go
+++ ch02/ex05/popcount_test.go
@@ -1,8 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"./pcount"
+)
 
 var pop = popCount
+
+func BenchmarkOriginalPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pcount.PopCount(1023)
+	}
+}
+
+func BenchmarkPopCountBitShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pop(1023)
+	}
+}
 
 func TestPopcount(t *testing.T) {
 	if pop(0) != 0 {
