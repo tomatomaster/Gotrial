08c938e9a8c8f9f17e315918bba10359d03c1d6a
tomatomaster <iiitomatomasteriii@gmail.com>
2017-08-17 13:47:24 +0900

    Implement ex08

---
+++ ch07/ex08/template.go
@@ -0,0 +1,52 @@
+// Copyright © 2017 Ryutarou Ono.
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"sort"
+)
+
+const templ = `<table>
+<tr style='text-align: left'>
+<th><a href="?sort=title">Title</a></th>
+<th><a href="?sort=artist">Artist</a></th>
+<th><a href="?sort=album">Album</a></th>
+<th><a href="?sort=year">Year</a></th>
+<th><a href="?sort=length">Length</a></th>
+</tr>
+{{range .}}<tr>
+<td>{{.Title}}</td>
+<td>{{.Artist}}</td>
+<td>{{.Album}}</td>
+<td>{{.Year}}</td>
+<td>{{.Length}}</td>
+</tr>
+{{end}}`
+
+var report = template.Must(template.New("trackList").Parse(templ))
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("sort")
+	switch query {
+	case "title":
+		sort.Sort(byTitle(tracks))
+	case "artist":
+		sort.Sort(byArtist(tracks))
+	case "album":
+		sort.Sort(byAlbum(tracks))
+	default:
+		fmt.Println("query has not defined yet or nil")
+	}
+	if err := report.Execute(w, tracks); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ ch07/ex08/tracks.go
@@ -0,0 +1,65 @@
+// Copyright © 2017 Ryutarou Ono.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+	"time"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Super", "Moby", "Moby", 1990, length("1m38s")},
+	{"Head", "Alicia", "Shut it up", 2011, length("5m38s")},
+	{"Bed", "Alicia", "night", 2010, length("5m38s")},
+	{"Alphabet", "Alicia", "abcd", 2015, length("5m38s")},
+	{"Tumiki", "01", "Sit", 451, length("10m38s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return d
+}
+
+func printTracks(track []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "-----", "-----", "-----", "-----")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byAlbum []*Track
+
+func (x byAlbum) Len() int           { return len(x) }
+func (x byAlbum) Less(i, j int) bool { return x[i].Album < x[j].Album }
+func (x byAlbum) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byTitle []*Track
+
+func (x byTitle) Len() int           { return len(x) }
+func (x byTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
+func (x byTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
